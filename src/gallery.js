7a5f9a7df90108e51689a961e958b6c56b5b7fb7
soungho_cho <soungho_cho@tmax.co.kr>
2024-09-05 08:55:51 +0900

    image로직 리스트화

--- src/gallery.js
+++ src/gallery.js
@@ -8,40 +8,15 @@ import csh6 from './csh6.jpg';
 import csh7 from './csh7.jpg';
 import csh8 from './csh8.jpg';
 
-const images = [
-    {
-        original: csh1,
-        thumbnail: csh1
-    },
-    {
-        original: csh2,
-        thumbnail: csh2
-    },
-    {
-        original: csh3,
-        thumbnail: csh3
-    },
-    {
-        original: csh4,
-        thumbnail: csh4
-    },
-    {
-        original: csh5,
-        thumbnail: csh5
-    },
-    {
-        original: csh6,
-        thumbnail: csh6
-    },
-    {
-        original: csh7,
-        thumbnail: csh7
-    },
-    {
-        original: csh8,
-        thumbnail: csh8
-    }
-];
+const images = [];
+images.push({ original: csh1, thumbnail: csh1});
+images.push({ original: csh2, thumbnail: csh2});
+images.push({ original: csh3, thumbnail: csh3});
+images.push({ original: csh4, thumbnail: csh4});
+images.push({ original: csh5, thumbnail: csh5});
+images.push({ original: csh6, thumbnail: csh6});
+images.push({ original: csh7, thumbnail: csh7});
+images.push({ original: csh8, thumbnail: csh8});
 
 function MyGallery() {
     return(
