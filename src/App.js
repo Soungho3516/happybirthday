d0a98021a532c0069c2c24fb141a70ad88d86baa
soungho_cho <soungho_cho@tmax.co.kr>
2024-09-05 17:15:45 +0900

    fix typo

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
   return (
     <div className="App">
       <div className='App-subtitle'>
-        화면을 터치하면 화면이 재생되요~
-        </div>
+        화면을 터치하면 음악이 재생되요~    
       <AudioPlayer></AudioPlayer>
+        </div>
         <div className="App-header">
           <img src={csh9} style={{width: '100%'}}></img>
         <div style={{width: '90%', alignSelf:'center', maxWidth: '500px'}}>
