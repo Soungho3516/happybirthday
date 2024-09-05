ad27d790977f3c71fe53dd65080300731e234a51
soungho_cho <soungho_cho@tmax.co.kr>
2024-09-05 15:12:24 +0900

    add music

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,22 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import mp3MR from './happybirthdayMR.mp3'
 
 const AudioPlayer = () => {
-  // useRef를 사용하여 audio 엘리먼트에 접근합니다.
   const audioRef = useRef(null);
 
   useEffect(() => {
-    // 컴포넌트가 마운트될 때 오디오를 재생합니다.
-    if (audioRef.current) {
-      audioRef.current.play().catch((error) => {
-        console.log('Autoplay prevented:', error);
-        // 자동 재생이 차단될 경우 사용자가 직접 상호작용하도록 유도할 수 있습니다.
-      });
-    }
+    // 페이지의 클릭 이벤트를 리스닝합니다.
+    const handleUserInteraction = () => {
+      if (audioRef.current) {
+        audioRef.current.play().catch((error) => {
+          console.log('Play failed:', error);
+        });
+      }
+    };
+
+    // 페이지에서 클릭, 키보드 입력 등의 이벤트를 추가합니다.
+    window.addEventListener('click', handleUserInteraction);
+    window.addEventListener('keydown', handleUserInteraction);
+
+    // 컴포넌트가 언마운트 될 때 이벤트 리스너를 제거합니다.
+    return () => {
+      window.removeEventListener('click', handleUserInteraction);
+      window.removeEventListener('keydown', handleUserInteraction);
+    };
   }, []);
 
   return (
-    <div>
-      <audio ref={audioRef} src="./happybirthdayMR.mp3" autoPlay />
+    <div style={{backgroundColor: '#FFF8EE'}}>
+      <audio ref={audioRef} src={mp3MR}>
+        <source src="/audio/your-audio-file.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+      <p>화면을 터치하면 음악이 재생되요~</p>
     </div>
   );
 };
